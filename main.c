6d6351bc2e335fa682f5fcf3b89e8796591c0c64
Toprak Balci <tbalci@k1m05s06.42istanbul.com.tr>
2023-12-13 19:33:42 +0300

    m

---
+++ main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbalci <tbalci@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 18:19:26 by tbalci            #+#    #+#             */
+/*   Updated: 2023/12/13 18:31:20 by tbalci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(int ac, char **av, char **env)
+{
+    char *read_line;
+
+    while (1)
+    {
+        read_line = readline("minishell> ");
+        free(read_line);
+		
+    }
+
+    return 0;
+}
